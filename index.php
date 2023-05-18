6a3ab56b22fce93bcca89bc587ddce57433da9e7
Arthur Duarte <arthurduartealves99@gmail.com>
2023-05-17 23:42:35 -0300

    Configurando o projeto

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+require_once("config/url.php");
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+  <title>Título da página</title>
+  <link rel="stylesheet" href="<?= $BASE_URL ?>css/styles.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/css/bootstrap-grid.min.css" integrity="sha512-JQksK36WdRekVrvdxNyV3B0Q1huqbTkIQNbz1dlcFVgNynEMRl0F8OSqOGdVppLUDIvsOejhr/W5L3G/b3J+8w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300&family=Open+Sans:wght@300;700&display=swap" rel="stylesheet">
+  <meta charset="utf-8">
+</head>
+
+<body>
+  <h1>Testando</h1>
+  <i class="fas fa-eye">
+  </i>
+</body>
+
+</html>
