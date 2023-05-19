fc576946007c74fdc4b0ec685684870052831d47
Arthur Duarte <arthurduartealves99@gmail.com>
2023-05-18 23:20:24 -0300

    Personalizando a home e criando pagina de contato

--- create.php
+++ create.php
@@ -1,10 +1,7 @@
  <?php
   require_once('templat/header.php')
   ?>
-
- <h1>Testando</h1>
- <i class="fas fa-eye">
- </i>
+<h1>Criando contato</h1>
 
  <?php
   require_once('templat/footer.php')
---
+++ templat/header.php
@@ -0,0 +1,26 @@
+<?php
+require_once("config/url.php");
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+  <title>Título da página</title>
+  <link rel="stylesheet" href="<?= $BASE_URL ?>css/styles.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300&family=Open+Sans:wght@300;700&display=swap" rel="stylesheet">
+  <meta charset="utf-8">
+</head>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+    <a class="navbar-brand" href="<?= $BASE_URL ?>index.php">
+    <img src="<?= $BASE_URL ?>img/agenda.png" alt="agenda">
+    <div class="navbar-nav">
+      <a class="nav-link active" id="home-link" href="<?= $BASE_URL ?>index.php">Agenda</a>
+      <a class="nav-link active" id="home-link" href="<?= $BASE_URL ?>create.php">Adicionar Contato</a>
+    </div>
+    </a>
+  </nav>
+<body>
