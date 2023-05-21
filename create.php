f2cbbf6b61540d1163df7333193a15bb48c09961
Arthur Duarte <arthurduartealves99@gmail.com>
2023-05-20 23:18:08 -0300

    Realizado a pagina de criação

--- create.php
+++ create.php
@@ -2,9 +2,30 @@
   require_once('templat/header.php')
   ?>
 
- <div class="container">
+ <div class="container1">
    <?php require_once('templat/backbtn.html'); ?>
-   <h1>Criando contato</h1>
+   <h1 id="main-title">Criando contato</h1>
+   <form id="create-form" action="<?= $BASE_URL ?>config/process.php" method="post">
+     <input type="hidden" name="type" value="create">
+     <div class="form-group">
+       <label for="name">Nome do contato</label>
+       <input type="text" class="form-control" id="name" name="name" placeholder="Digite o nome" autocomplete="off" required>
+     </div>
+
+     <div class="form-group">
+       <label for="phone">Telefone do contato</label>
+       <input type="text" class="form-control" id="phone" name="phone" placeholder="Digite o telefone" autocomplete="off" required>
+     </div>
+    
+     <div class="form-group">
+       <label for="phone">Informações do contato</label>
+       <textarea type="text" class="form-control" id="phone" name="phone" rows="3" placeholder="Digite as informações do contato" autocomplete="off"></textarea>
+     </div>
+
+     <br/>
+     <button type="submit" class="btn btn-primary">Cadastrar</button>
+
+   </form>
 
  </div>
  <?php
